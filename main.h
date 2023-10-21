946766c4e0756379e01dd6bf0bb20a6518aa52b6
youssef <youwe2097@gmail.com>
2023-10-20 19:44:40 -0700

    printf

---
+++ main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <string.h>
+#include <limits.h>
+
+
+
+
+int _printf(const char *format, ...);
+int _putchar(char c);
+int _printf_string(char *cont);
+#endif
